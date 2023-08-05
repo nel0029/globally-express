52df6ce06036ba711c59af1aba50ebe17b682ca4
jaynel29 <jaynellucanas@gmail.com>
2023-08-05 10:37:28 +0800

    BgColor for repost parent card is added

--- backend/controllers/PostControllers/updateReply.js
+++ backend/controllers/PostControllers/updateReply.js
@@ -1,38 +1,31 @@
-const { Users } = require('../../models/userModel')
-const { Replies } = require('../../models/postsModel')
-const asyncHandler = require('express-async-handler')
+/** @format */
 
+const { Users } = require("../../models/userModel");
+const { Replies } = require("../../models/postsModel");
+const asyncHandler = require("express-async-handler");
 
 const updateReply = asyncHandler(async (req, res) => {
+  const { authorID, _id, caption } = req.body;
+  const userExists = await Users.findById(authorID);
 
-    const { authorID, postID, caption } = req.body
-    const userExists = await Users.findById(authorID)
+  if (userExists) {
+    const replyExists = await Replies.findById(_id);
 
-    if (userExists) {
+    if (replyExists) {
+      if (String(replyExists.authorID) === String(userExists._id)) {
+        replyExists.caption = caption;
+        await replyExists.save();
 
-        const replyExists = await Replies.findById(postID)
-
-        if (replyExists) {
-            if (String(replyExists.authorID) === String(userExists._id)) {
-                replyExists.caption = caption;
-                await replyExists.save();
-
-                res.status(202).json(replyExists);
-
-            } else {
-
-                res.status(401).json({ message: "Unauthorized request" })
-            }
-
-        } else {
-
-            res.status(404).json({ message: "Reply not found" })
-        }
-
+        res.status(202).json(replyExists);
+      } else {
+        res.status(401).json({ message: "Unauthorized request" });
+      }
     } else {
-
-        res.status(404).json({ message: "User not found" })
+      res.status(404).json({ message: "Reply not found" });
     }
-})
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+});
 
-module.exports = updateReply
+module.exports = updateReply;
--- backend/controllers/PostControllers/updateRepost.js
+++ backend/controllers/PostControllers/updateRepost.js
@@ -1,34 +1,30 @@
-const { Users } = require('../../models/userModel')
-const { Reposts } = require('../../models/postsModel')
-const asyncHandler = require('express-async-handler')
+/** @format */
 
+const { Users } = require("../../models/userModel");
+const { Reposts } = require("../../models/postsModel");
+const asyncHandler = require("express-async-handler");
 
 const updateRepost = asyncHandler(async (req, res) => {
+  const { authorID, _id, caption } = req.body;
+  const userExists = await Users.findById(authorID);
+  console.log(_id);
+  if (userExists) {
+    const repostExists = await Reposts.findById(_id);
+    if (repostExists) {
+      if (repostExists.authorID.toString() === String(userExists._id)) {
+        repostExists.caption = caption;
+        await repostExists.save();
 
-    const { authorID, postID, caption } = req.body
-    const userExists = await Users.findById(authorID)
-
-    if (userExists) {
-
-        const repostExists = await Reposts.findById(postID)
-        if (repostExists) {
-            if (repostExists.authorID.toString() === String(userExists._id)) {
-                repostExists.caption = caption;
-                await repostExists.save();
-
-                res.status(202).json(repostExists);
-
-            } else {
-                res.status(401).json({ message: "Unauthorized request" })
-            }
-
-        } else {
-            res.status(404).json({ message: "Repost not found" })
-        }
-
+        res.status(202).json(repostExists);
+      } else {
+        res.status(401).json({ message: "Unauthorized request" });
+      }
     } else {
-        res.status(404).json({ message: "User not found" })
+      res.status(404).json({ message: "Repost not found" });
     }
-})
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+});
 
-module.exports = updateRepost
+module.exports = updateRepost;
