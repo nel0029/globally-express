147a29c1e3d7c03f5fe1491ee5eaa6729b337b1d
jaynel29 <jaynellucanas@gmail.com>
2023-08-13 11:06:36 +0800

    ReadUserFollowing and ReadUserFollowing is been updated

--- backend/controllers/PostControllers/createNewLike.js
+++ backend/controllers/PostControllers/createNewLike.js
@@ -1,68 +1,65 @@
-const { Posts, Replies, Reposts, Likes } = require('../../models/postsModel')
-const { Users } = require('../../models/userModel')
-const asyncHandler = require('express-async-handler')
+/** @format */
+
+const { Posts, Replies, Reposts, Likes } = require("../../models/postsModel");
+const { Users } = require("../../models/userModel");
+const asyncHandler = require("express-async-handler");
 
 const createNewLike = asyncHandler(async (req, res) => {
+  const { authorID, postID, parentType, parentAuthorID } = req.body;
 
-    const { authorID, postID, parentType, parentAuthorID } = req.body
+  const userExists = await Users.findById(authorID);
 
-    const userExists = await Users.findById(authorID)
+  if (userExists) {
+    let parentExists;
 
-    if (userExists) {
-
-        let parentExists
-
-        switch (parentType) {
-            case 'post':
-                parentExists = await Posts.findOne({ _id: postID })
-                break
-            case 'reply':
-                parentExists = await Replies.findOne({ _id: postID })
-                break
-            case 'repost':
-                parentExists = await Reposts.findOne({ _id: postID })
-                break
-            default: res.status(400).json({ message: "Invalid Parent Typpe" })
-        }
-
-        const likeExists = await Likes.findOne({ $and: [{ _id: postID }, { authorID: String(userExists._id) }] });
-
-        if (likeExists) {
-
-            res.status(400).json({ message: "You already like this post" })
-
-        } else {
-
-            if (parentExists) {
-                const newLike = new Likes({
-                    authorID: userExists._id,
-                    parentPostID: parentExists._id,
-                    parentType: parentExists.type,
-                    type: 'like',
-                    parentAuthorID: parentAuthorID
-                });
-
-                await newLike.save();
-
-                await parentExists.updateOne({ $inc: { likesCount: 1 } });
-                const reponseLike = {
-                    _id: newLike._id.toString(),
-                    postAuthorID: newLike.authorID,
-                    parentID: newLike.parentPostID,
-                    parentType: newLike.parentType,
-                    type: newLike.type
-                }
-                res.status(201).json(reponseLike)
-            } else {
-                res.status(404).json({ message: "Post not found" })
-            }
-        }
-
-    } else {
-        res.status(404).json({ message: 'User not found.' });
+    switch (parentType) {
+      case "post":
+        parentExists = await Posts.findOne({ _id: postID });
+        break;
+      case "reply":
+        parentExists = await Replies.findOne({ _id: postID });
+        break;
+      case "repost":
+        parentExists = await Reposts.findOne({ _id: postID });
+        break;
+      default:
+        res.status(400).json({ message: "Invalid Parent Typpe" });
     }
 
+    const likeExists = await Likes.findOne({
+      $and: [{ _id: postID }, { authorID: String(userExists._id) }],
+    });
 
-})
+    if (likeExists) {
+      res.status(400).json({ message: "You already like this post" });
+    } else {
+      if (parentExists) {
+        const newLike = new Likes({
+          authorID: userExists._id,
+          parentPostID: parentExists._id,
+          parentType: parentExists.type,
+          type: "like",
+          parentAuthorID: parentAuthorID,
+        });
 
-module.exports = createNewLike
+        await newLike.save();
+
+        await parentExists.updateOne({ $inc: { likesCount: 1 } });
+        const reponseLike = {
+          _id: newLike._id.toString(),
+          postAuthorID: newLike.authorID,
+          parentID: newLike.parentPostID,
+          parentType: newLike.parentType,
+          type: newLike.type,
+        };
+        res.status(201).json(reponseLike);
+      } else {
+        res.status(404).json({ message: "Post not found" });
+      }
+    }
+  } else {
+    res.status(404).json({ message: "User not found." });
+  }
+});
+
+module.exports = createNewLike;
--- backend/controllers/UsersController/ReadUserFollowing.js
+++ backend/controllers/UsersController/ReadUserFollowing.js
@@ -1,83 +1,101 @@
-const asyncHandler = require('express-async-handler');
-const { Users, Following } = require('../../models/userModel');
-const { ObjectId } = require('mongodb');
+/** @format */
+
+const asyncHandler = require("express-async-handler");
+const { Users, Following } = require("../../models/userModel");
+const { ObjectId } = require("mongodb");
 
 const ReadUserFollowing = asyncHandler(async (req, res) => {
-    const { userName } = req.params
-    const { userID } = req.query
+  const { userName } = req.params;
+  const { userID } = req.query;
 
-    const userExists = await Users.findOne({ userName: userName })
+  const userExists = await Users.findOne({ userName: userName });
 
-    if (userExists) {
-        const userFollowing = await Following.aggregate([
-            {
-                $match: {
-                    followerID: userExists._id
-                }
+  if (userExists) {
+    const userFollowing = await Following.aggregate([
+      {
+        $match: {
+          followerID: userExists._id,
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "followingID",
+          foreignField: "_id",
+          as: "users",
+        },
+      },
+      {
+        $unwind: "$users",
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "followerID",
+          foreignField: "_id",
+          as: "userFollower",
+        },
+      },
+      {
+        $unwind: "$userFollower",
+      },
+      {
+        $lookup: {
+          from: "followings",
+          localField: "users._id",
+          foreignField: "followingID",
+          as: "followers",
+        },
+      },
+      {
+        $lookup: {
+          from: "followings",
+          localField: "users._id",
+          foreignField: "followerID",
+          as: "followings",
+        },
+      },
+      {
+        $project: {
+          followerID: 1,
+          userFollowerUserName: "$userFollower.userName",
+          _id: "$users._id",
+          userName: "$users.userName",
+          userFirstName: "$users.userFirstName",
+          userMiddleName: "$users.userMiddleName",
+          userLastName: "$users.userLastName",
+          avatarURL: "$users.avatarURL",
+          coverPhotoURL: "$users.coverPhotoURL",
+          bio: "$users.bio",
+          followersCount: { $size: "$followers" },
+          followingCount: { $size: "$followings" },
+          isUserFollowed: {
+            $in: [new ObjectId(userID), "$followers.followerID"],
+          },
+          followID: {
+            $let: {
+              vars: {
+                matchedFollowers: {
+                  $filter: {
+                    input: "$followers",
+                    as: "follower",
+                    cond: {
+                      $eq: ["$$follower.followerID", new ObjectId(userID)],
+                    },
+                  },
+                },
+              },
+              in: { $arrayElemAt: ["$$matchedFollowers._id", 0] },
             },
-            {
-                $lookup: {
-                    from: "users",
-                    localField: "followingID",
-                    foreignField: "_id",
-                    as: "users"
-                }
-            },
-            {
-                $unwind: "$users"
-            },
-            {
-                $lookup: {
-                    from: "followings",
-                    localField: "users._id",
-                    foreignField: "followingID",
-                    as: "followers"
-                }
-            },
-            {
-                $lookup: {
-                    from: "followings",
-                    localField: "users._id",
-                    foreignField: "followerID",
-                    as: "followings"
-                }
-            },
-            {
-                $project: {
-                    _id: "$users._id",
-                    userName: "$users.userName",
-                    userFirstName: "$users.userFirstName",
-                    userMiddleName: "$users.userMiddleName",
-                    userLastName: "$users.userLastName",
-                    avatarURL: "$users.avatarURL",
-                    coverPhotoURL: "$users.coverPhotoURL",
-                    bio: "$users.bio",
-                    followersCount: { $size: "$followers" },
-                    followingCount: { $size: "$followings" },
-                    isUserFollowed: { $in: [new ObjectId(userID), "$followers.followerID"] },
-                    followID: {
-                        $let: {
-                            vars: {
-                                matchedFollowers: {
-                                    $filter: {
-                                        input: "$followers",
-                                        as: "follower",
-                                        cond: { $eq: ["$$follower.followerID", new ObjectId(userID)] }
-                                    }
-                                }
-                            },
-                            in: { $arrayElemAt: ["$$matchedFollowers._id", 0] }
-                        }
-                    }
-                }
-            }
-        ])
+          },
+        },
+      },
+    ]);
 
-        res.status(200).json(userFollowing)
-    } else {
-        res.status(404).json({ message: "User Not Found" })
-    }
+    res.status(200).json(userFollowing);
+  } else {
+    res.status(404).json({ message: "User Not Found" });
+  }
+});
 
-})
-
-module.exports = ReadUserFollowing
+module.exports = ReadUserFollowing;
