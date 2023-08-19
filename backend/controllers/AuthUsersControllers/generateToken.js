7a28d1b619df1c8972a32906e2f68b33bbcfb241
jaynel29 <jaynellucanas@gmail.com>
2023-08-19 23:43:44 +0800

    Tokens are now set in httpOnly cookie

--- backend/controllers/AuthUsersControllers/generateToken.js
+++ backend/controllers/AuthUsersControllers/generateToken.js
@@ -11,7 +11,7 @@ const generateToken = (res, id) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
     sameSite: "lax",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
